40f633c6dad6e7091f7bb857649f41ee4b867313
MatveySviadysh <mkomp06@gmail.com>
2024-11-09 21:02:36 +0300

    add more documentation to code

--- app/routes/city.py
+++ app/routes/city.py
@@ -1,11 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException, Query
-from pytest import Session
-from database.models import City
-from models.city_model import CityCreate, CityResponse
-from utils.classes import CityBase
-from utils.helpers import get_db
-from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
+from database.models import City
 from models.city_model import CityCreate, CityResponse
 from utils.helpers import get_db
 
@@ -13,6 +8,15 @@ city_router = APIRouter()
 
 @city_router.post("/city/create", response_model=CityResponse)
 async def create_city(city: CityCreate, db: Session = Depends(get_db)) -> CityResponse:
+    """
+    Создает новый город.
+    
+    - **name**: Название города
+    - **description**: Описание города
+    - **image_url**: URL изображения города
+
+    Возвращает созданный объект города.
+    """
     existing_city = db.query(City).filter(City.name == city.name).first()
     if existing_city:
         raise HTTPException(status_code=400, detail="City with this name already exists.")
@@ -29,16 +33,28 @@ async def create_city(city: CityCreate, db: Session = Depends(get_db)) -> CityRe
 
 @city_router.get("/cities", response_model=list[CityResponse])
 async def get_cities(db: Session = Depends(get_db)) -> list[City]:
+    """
+    Получает список всех городов.
+    Возвращает список объектов городов.
+    """
     cities = db.query(City).all()
     return cities
 
 
 @city_router.get("/city", response_model=CityResponse)
 async def search_city(
-        id: int = Query(None, ge=1, le=50),
-        filter: str = Query(None),
-        db: Session = Depends(get_db)
-    ) -> CityResponse:
+        id: int = Query(None, ge=1, le=50, description="ID города для поиска. Должен быть в пределах от 1 до 50."),
+        filter: str = Query(None, description="Часть названия города для фильтрации.")
+    , db: Session = Depends(get_db)) -> CityResponse:
+    """
+    Ищет город по ID или по фильтру.
+    Вы можете передать либо `id`, либо `filter` для выполнения поиска.
+    
+    - **id**: ID города в базе данных.
+    - **filter**: Строка для фильтрации городов по названию.
+
+    Если город не найден, возвращает ошибку 404.
+    """
     query = db.query(City)
     if id is not None:
         city = query.filter(City.id == id).first()
@@ -52,6 +68,13 @@ async def search_city(
 
 @city_router.delete("/city/{city_id}", response_model=dict)
 async def delete_city(city_id: int, db: Session = Depends(get_db)) -> dict:
+    """
+    Удаляет город по ID.
+
+    - **city_id**: ID удаляемого города.
+
+    Возвращает сообщение об успешном удалении.
+    """
     city = db.query(City).filter(City.id == city_id).first()
     if city is None:
         raise HTTPException(status_code=404, detail="City not found")
@@ -60,15 +83,22 @@ async def delete_city(city_id: int, db: Session = Depends(get_db)) -> dict:
     return {"message": "City deleted successfully"}
 
 
-
 @city_router.put("/city/{city_id}", response_model=CityResponse)
 async def update_city(city_id: int, city: CityCreate, db: Session = Depends(get_db)) -> CityResponse:
+    """
+    Обновляет информацию о городе.
+
+    - **city_id**: ID города для обновления.
+    - **city**: Объект CityCreate с обновляемыми данными.
+
+    Возвращает обновленный объект города.
+    """
     db_city = db.query(City).filter(City.id == city_id).first()
     if not db_city:
         raise HTTPException(status_code=404, detail="City not found.")
-    db_city.name = city.name # type: ignore
-    db_city.description = city.description # type: ignore
-    db_city.image_url = city.image_url # type: ignore
+    db_city.name = city.name  # type: ignore
+    db_city.description = city.description  # type: ignore
+    db_city.image_url = city.image_url  # type: ignore
     db.commit()
     db.refresh(db_city)
     return db_city
--- app/routes/tour_guide.py
+++ app/routes/tour_guide.py
@@ -1,23 +1,24 @@
 from typing import List
 from fastapi import APIRouter, Depends, HTTPException
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, joinedload
 from database.models import TourGuide, City
 from models.tourguide_model import TourGuideCreate, TourGuideResponse
 from utils.helpers import get_db
-from sqlalchemy.orm import joinedload
 
 tour_guide_router = APIRouter()
 
-
 @tour_guide_router.post('/tour_guide/create', response_model=TourGuideResponse)
-async def create_tour_guide(tour_guide: TourGuideCreate, db: Session = Depends(get_db)) ->TourGuide:
+async def create_tour_guide(tour_guide: TourGuideCreate, db: Session = Depends(get_db)) -> TourGuide:
+    """
+    Создает нового гида по турам.
+    Проверяет, существует ли гид с таким же именем и есть ли указанный город.
+    """
     existing_tour_guide = db.query(TourGuide).filter(TourGuide.name == tour_guide.name).first()
     if existing_tour_guide:
         raise HTTPException(status_code=400, detail="Tour guide with this name already exists.")
     city = db.query(City).filter(City.id == tour_guide.city_id).first()
     if not city:
         raise HTTPException(status_code=400, detail="City not found.")
-    
     db_tour_guide = TourGuide(
         name=tour_guide.name,
         experience_years=tour_guide.experience_years,
@@ -30,39 +31,52 @@ async def create_tour_guide(tour_guide: TourGuideCreate, db: Session = Depends(g
     db.refresh(db_tour_guide)
     return db_tour_guide
 
-
 @tour_guide_router.get('/tour_guides/', response_model=List[TourGuideResponse])
 def read_tour_guides(db: Session = Depends(get_db)) -> List[TourGuide]:
+    """
+    Возвращает список всех гидов по турам, включая информацию о городе.
+    Использует joinedload для оптимизации запросов и предотвращения проблемы N + 1.
+    """
     return db.query(TourGuide).options(joinedload(TourGuide.city)).all()
-
 
 @tour_guide_router.get('/tour_guide/{id}', response_model=TourGuideResponse)
 async def search_tour_guide(id: int, db: Session = Depends(get_db)):
-    tour_guide = db.query(TourGuide).filter(TourGuide.id == id).first()
+    """
+    Находит гида по ID.
+    Возвращает 404 ошибку, если гид с указанным ID не найден.
+    Также загружает информацию о городе.
+    """
+    tour_guide = db.query(TourGuide).options(joinedload(TourGuide.city)).filter(TourGuide.id == id).first()
     if not tour_guide:
         raise HTTPException(status_code=404, detail="Tour guide not found")
     return tour_guide
 
-
 @tour_guide_router.delete('/tour_guide/{id}')
 async def delete_tour_guide(id: int, db: Session = Depends(get_db)):
+    """
+    Удаляет гида по указанному ID.
+    Возвращает сообщение об успешном удалении или 404 ошибку, если гид не найден.
+    """
     tour_guide = db.query(TourGuide).filter(TourGuide.id == id).first()
     if not tour_guide:
         raise HTTPException(status_code=404, detail="Tour guide not found")
     db.delete(tour_guide)
     db.commit()
-    return {"message": "tour_guide deleted successfully"}
-
+    return {"message": "Tour guide deleted successfully"}
 
 @tour_guide_router.put('/tour_guide/{id}', response_model=TourGuideResponse)
 async def update_tour_guide(id: int, tour_guide: TourGuideCreate, db: Session = Depends(get_db)):
+    """
+    Обновляет информацию о гиде по указанному ID.
+    Возвращает 404 ошибку, если гид не найден.
+    """
     db_tour_guide = db.query(TourGuide).filter(TourGuide.id == id).first()
     if not db_tour_guide:
         raise HTTPException(status_code=404, detail="Tour guide not found")
-    db_tour_guide.name = tour_guide.name # type: ignore
-    db_tour_guide.experience_years = tour_guide.experience_years # type: ignore
-    db_tour_guide.bio = tour_guide.bio # type: ignore
-    db_tour_guide.contact_info = tour_guide.contact_info # type: ignore
+    db_tour_guide.name = tour_guide.name  # type: ignore
+    db_tour_guide.experience_years = tour_guide.experience_years  # type: ignore
+    db_tour_guide.bio = tour_guide.bio  # type: ignore
+    db_tour_guide.contact_info = tour_guide.contact_info  # type: ignore
     db.commit()
     db.refresh(db_tour_guide)
     return db_tour_guide
