19c8d12e445318d11c7a1bbe27d81e0cba1e6b0b
MatveySviadysh <mkomp06@gmail.com>
2024-11-10 13:06:57 +0300

    crud travel

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from routes.user import user_router
 from routes.city import city_router
+from routes.travel import travel_router
 from routes.tour_guide import tour_guide_router
 app = FastAPI()
 
 app.include_router(user_router)
 app.include_router(city_router)
 app.include_router(tour_guide_router)
+app.include_router(travel_router)
